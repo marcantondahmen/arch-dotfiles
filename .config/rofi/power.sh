525b423d4036dc0a56f0f8a8c50e6659a1e9aea5
Marc Anton Dahmen <mail@marcdahmen.de>
2024-07-25 10:20:49 +0200

    minor fixes

--- .config/rofi/power.sh
+++ .config/rofi/power.sh
@@ -4,7 +4,7 @@ chosen=$(printf "Lock\nSleep\nReboot\nShutdown\nLog Out" | rofi -dmenu -i)
 
 case "$chosen" in
 "Lock") bash ~/.config/i3/lock.sh ;;
-"Sleep") systemctl suspend-then-hibernate ;;
+"Sleep") systemctl suspend ;;
 "Reboot") reboot ;;
 "Shutdown") poweroff ;;
 "Log Out") i3-msg exit ;;
