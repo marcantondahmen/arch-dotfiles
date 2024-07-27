6615cba11c78cf7527af5f8ccb5979997ea43aef
Marc Anton Dahmen <mail@marcdahmen.de>
2024-07-27 13:21:06 +0200

    minor fixes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.front_end = "WebGpu"
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
